1a1ee5f1574853556e28f30f44ce363b82ca5bf4
Jean-Pierre <Olwagenjp@gmail.com>
2018-03-07 08:50:17 +0200

    Added top and bottom navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TopNavigationComponent } from './components/top-navigation/top-navigation.component';
 import { MaterialDesignModule } from './material-design/material-design.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+import { TeamViewComponent } from './components/team-view/team-view.component';
+import { BottomNavigationComponent } from './components/bottom-navigation/bottom-navigation.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavigationComponent
+    TopNavigationComponent,
+    TeamViewComponent,
+    BottomNavigationComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MaterialDesignModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
